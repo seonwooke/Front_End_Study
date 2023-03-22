5300e1949d67f36b572e1ff1937f5eaacf3aadfc
seonwooke <seonookkim@gmail.com>
2023-03-22 12:01:14 +0900

    :tada: Java Data Begin
    Java Data Begin

---
+++ course1-part4-javascript/nodejs/main.ts
@@ -0,0 +1,15 @@
+import _ from 'lodash'
+
+console.log(_.upperCase('hello-world'))
+
+interface User {
+    name: string
+    age: number
+}
+
+const user: User = {
+    name: 'Seonwook Kim',
+    age: 27
+}
+
+console.log(user)
